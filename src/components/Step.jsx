5d0ced11c3741632a997fad854cc6bacd9be7e59
Bloemenstraat <del.crabman@gmail.com>
2023-12-29 22:28:48 +0100

    Mobile responsive version

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -2,13 +2,19 @@ import './Step.css'
 
 export default function Step ({ i, text, selected }) {
 
+    let mobile = false;
+
+    if (document.documentElement.clientWidth <= 375)
+        mobile = true;
+
+
     return (
         <div className="step">
             <div className={ selected ? "selected-number" : "number" }>{i}</div>
-            <div className="description">
+            {!mobile && <div className="description">
                 <p>Step {i}</p>
                 <p>{ text }</p>
-            </div>
+            </div>}
         </div>
     );
 }
